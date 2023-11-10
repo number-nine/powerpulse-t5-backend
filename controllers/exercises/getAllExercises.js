35f440f19861339be0104dd21a71905ecf0b71fc
brizhak <ada.adelina17@gmail.com>
2023-11-10 17:10:16 +0200

    update getAllExercises and paginationParams

--- controllers/exercises/getAllExercises.js
+++ controllers/exercises/getAllExercises.js
@@ -1,8 +1,11 @@
 const { ctrlWrapper } = require("../../helpers/index.js");
 const { Exercise } = require("../../models/exercise.js");
+const paginationParams = require("../../helpers/paginationParams.js");
 
 const getAllExercises = async (req, res) => {
-  const result = await Exercise.find();
+  const { page, limit } = req.query;
+  const { skip, limit: normalizedLimit } = paginationParams(page, limit);
+  const result = await Exercise.find().skip(skip).limit(normalizedLimit);
   res.json(result);
 };
 
