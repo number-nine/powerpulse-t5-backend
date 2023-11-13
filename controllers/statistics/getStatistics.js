af28d286ded1c22af6ba5f9967c55b24d96f4db7
brizhak <ada.adelina17@gmail.com>
2023-11-13 19:31:58 +0200

    Update getStatistics.js

--- controllers/statistics/getStatistics.js
+++ controllers/statistics/getStatistics.js
@@ -3,34 +3,51 @@ const { User } = require("../../models/user.js");
 const { Workout } = require("../../models/workout.js");
 
 const getStatistics = async (req, res) => {
-  const userCount = await User.countDocuments();
+  // const userCount = await User.countDocuments();
 
-  const workoutCount = await Workout.countDocuments();
+  // const workoutCount = await Workout.countDocuments();
 
-  const burnedCalories = await Workout.find({}, "burnedCalories");
+  // const burnedCalories = await Workout.find({}, "burnedCalories");
 
-  const burnedCaloriesValues = burnedCalories.map(
-    (workout) => workout.burnedCalories
-  );
-  const burnedCaloriesSum = burnedCaloriesValues.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
+  // const burnedCaloriesValues = burnedCalories.map(
+  //   (workout) => workout.burnedCalories
+  // );
+  // const burnedCaloriesSum = burnedCaloriesValues.reduce(
+  //   (accumulator, currentValue) => accumulator + currentValue,
+  //   0
+  // );
 
-  const workoutsTime = await Workout.find({}, "time");
-  console.log(workoutsTime);
-  const workoutsTimeValues = workoutsTime.map((workout) => workout.time);
-  const workoutsTimeSum = workoutsTimeValues.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
+  // const workoutsTime = await Workout.find({}, "time");
+  // const workoutsTimeValues = workoutsTime.map((workout) => workout.time);
+  // const workoutsTimeSum = workoutsTimeValues.reduce(
+  //   (accumulator, currentValue) => accumulator + currentValue,
+  //   0
+  // );
+  const aggregationPipeline = [
+    {
+      $group: {
+        _id: null,
+        workoutCount: { $sum: 1 },
+        burnedCaloriesSum: { $sum: "$burnedCalories" },
+        workoutsTimeSum: { $sum: "$time" },
+      },
+    },
+  ];
 
-  res.json({
-    userCount,
-    burnedCaloriesSum,
-    workoutCount,
-    workoutsTimeSum,
-  });
+  const statistics = await Workout.aggregate(aggregationPipeline);
+
+  if (statistics.length > 0) {
+    const userCount = await User.countDocuments();
+    res.json({ ...statistics[0], userCount });
+  } else {
+    const userCount = await User.countDocuments();
+    res.json({
+      userCount,
+      workoutCount: 0,
+      burnedCaloriesSum: 0,
+      workoutsTimeSum: 0,
+    });
+  }
 };
 
 module.exports = ctrlWrapper(getStatistics);
