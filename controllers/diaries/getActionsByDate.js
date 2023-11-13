8dbdbd171236a65e4b29751f9ede83e1ea44d979
number-nine <sergii.pokhylko@gmail.com>
2023-11-13 10:44:46 +0200

    get diary  point

--- controllers/diaries/getActionsByDate.js
+++ controllers/diaries/getActionsByDate.js
@@ -5,8 +5,10 @@ const { ctrlWrapper, HttpError } = require("../../helpers");
 const getActionsByDate = async (req, res) => {
   const { _id } = req.user;
   const { date } = req.params;
-  const workouts = await Workout.find({ owner_id: _id, date });
-  const meals = await Meal.find({ owner_id: _id, date });
+  const workouts = await Workout.find({ owner_id: _id, date }).populate(
+    "exercise_id"
+  );
+  const meals = await Meal.find({ owner_id: _id, date }).populate("product_id");
 
   if (!workouts || !meals) {
     throw HttpError(404, "Not found");
