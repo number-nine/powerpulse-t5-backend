402a71c6e218bce9f08389cb6c4c96b72b7856df
number-nine <sergii.pokhylko@gmail.com>
2023-11-13 16:02:04 +0200

    fix/populate

--- controllers/meals/getMealsByDate.js
+++ controllers/meals/getMealsByDate.js
@@ -4,7 +4,9 @@ const { ctrlWrapper, HttpError } = require("../../helpers");
 const getMealsByDate = async (req, res) => {
   const { _id } = req.user;
   const { date } = req.params;
-  const result = await Meal.find({owner_id: _id, date}).populate("product_id");
+  const result = await Meal.find({ owner_id: _id, date }).populate(
+    "product_id"
+  );
   if (!result) {
     throw HttpError(404, "Not found");
   }
