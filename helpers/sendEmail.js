870106e0fb1ae527202c04b5348257ab600694f6
number-nine <sergii.pokhylko@gmail.com>
2023-11-20 12:43:21 +0200

    fix/front url

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -4,7 +4,8 @@ const sendEmail = async ({ email, token }) => {
   const { BASE_URL, EMAIL_API } = process.env;
   const EMAIL_FROM = "sender@snowboards.com.ua";
   const client = new postmark.ServerClient(EMAIL_API);
-  const frontendVerifyBaseRoute = "vasylievyurii.github.io/PowerPulse2.0/";
+  const frontendVerifyBaseRoute =
+    "https://vasylievyurii.github.io/PowerPulse2.0/signup";
   const msg = {
     From: EMAIL_FROM,
     To: email,
